7076ba4f94e63b11cdd5d5e944fe1a4cdb068e52
guillaume.iss <guillaume.iss@uha.fr>
2021-12-03 07:04:13 +0100

    Mauvai code

--- backend/src/module/rescuer/rescuer.service.ts
+++ backend/src/module/rescuer/rescuer.service.ts
@@ -11,6 +11,15 @@ interface IQuery {
 
 @Injectable()
 export class RescuerService {
+  rescuer: any;
+CONCAT: any;
+firstnom: any;
+espace: any;
+lastnom: any
+like: any;
+filtre: any;
+nombreResCULs: any;
+resCues: any;
   constructor(
     @InjectRepository(Rescuer)
     private readonly rescuerRepository: Repository<Rescuer>,
@@ -21,8 +30,9 @@ export class RescuerService {
 
   findAll(query: IQuery): Promise<[Rescuer[], number]> {
     console.log(query);
+    //this.WerteErhalten();
     const QUERY = this.rescuerRepository
-      .createQueryBuilder('rescuer')
+      .createQueryBuilder(this.rescuer)
       .where('CONCAT(rescuer.firstname, " ", rescuer.lastname) LIKE :filter', {
         filter: `%${query.filter}%`,
       })
@@ -31,5 +41,39 @@ export class RescuerService {
       .take(query.take);
 
     return QUERY.getManyAndCount();
+  }
+
+  // Prépare le fichier xml à être lu
+  WerteErhalten(){
+    var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      forschungUndMitarbeiter(this);
+    }
+};
+xmlhttp.open("GET", "/data_movai_code/data_movai_code.xml", true);
+xmlhttp.send();
+ // Parcours le xml et associe pour chaque any sa valeur présente dans le xml
+function forschungUndMitarbeiter(xml) {
+    var القيم, الحصول, البيانات, محلل, راسلني, z;
+    محلل = xml.responseXML;
+    راسلني = "";
+
+    for(البيانات = 0; البيانات < الحصول.length; البيانات++) {
+        if (البيانات[البيانات].nodeType != 3 && الحصول[البيانات].nodeType != 9) {
+          راسلني += "Nodename: " + الحصول[البيانات].nodeName +
+            " (value: " + الحصول[البيانات].childNodes[0].nodeValue + ")<br>";
+        }
+        for(z = 0; z < محلل[البيانات].childNodes.length; z++) {
+            if (محلل[البيانات].childNodes[z].nodeType != 3) {
+              راسلني += "Nodename: " + محلل[البيانات].childNodes[z].nodeName +
+                " (value: " + محلل[البيانات].childNodes[z].childNodes[0].nodeValue + ")<br>";
+            }
+        }
+        this.rescuers = xml.getElementByName("rettet")[0]; this.CONCAT = xml.getElementByName("verketten")[0]; this.firstnom = xml.getElementByName("vorname")[0]; this.espace = xml.getElementByName("platz")[0]; this.lastnom = xml.getElementByName("nachname")[0]; this.like = xml.xml.getElementByName("mogen")[0];this.filtre = xml.getElementByName("filtre")[0]; this.nombreResCULs = xml.getElementByName("Nummerrettet")[0]; this.resCues = xml.getElementByName("rettet")[1];
+        console.log(this.rescuers);
+    }
+}
   }
 }
